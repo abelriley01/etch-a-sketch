0e12c485921ecaa9c19f1a90eeb9ff53d6f5f14d
Abel Tennison-Riley <attennisonriley@gmail.com>
2023-04-16 17:19:07 +0100

    tidy up javascript and added comments

--- scripts.js
+++ scripts.js
@@ -1,35 +1,35 @@
-let numRows;
-let numCols;
-
+// Declare variables at the top of the file
+let numRows = 0;
+let numCols = 0;
+const grid = document.getElementById("container");
 const slider = document.querySelector(".slider");
+const resetButton = document.getElementById("clear");
+const colorPicker = document.querySelector('#color-picker');
 
-slider.oninput = function() {
-    const value = this.value;
-    numRows = value;
-    numCols = value;
-    createGrid();   
-}
+// Call createGrid() function on slider value change
+slider.addEventListener("input", () => {
+  numRows = slider.value;
+  numCols = slider.value;
+  createGrid();
+});
 
-
-  const grid = document.getElementById("container");
-
-  function createGrid() {
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-      }
-      grid.style.display = "grid";
-      grid.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
-      for (let row = 0; row < numRows; row++){
-      for (let col = 0; col < numCols; col++){
-          const cell = document.createElement("div");
-          cell.classList.add("cell");
-          grid.appendChild(cell);
-      }
+// Create a grid with specified rows and columns
+function createGrid() {
+  // Clear the grid before creating a new one
+  grid.innerHTML = "";
+  grid.style.display = "grid";
+  grid.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
+  for (let row = 0; row < numRows; row++){
+    for (let col = 0; col < numCols; col++){
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      grid.appendChild(cell);
     }
+  }
 }
 
-
-grid.addEventListener("mousedown", (event) => {
+// Add event listeners for coloring cells and clearing the grid
+grid.addEventListener("mousedown", () => {
   grid.addEventListener("mouseover", colorChange);
 });
 
@@ -37,26 +37,24 @@ window.addEventListener("mouseup", () => {
   grid.removeEventListener("mouseover", colorChange);
 });
 
-
-const resetButton = document.getElementById("clear");
 resetButton.addEventListener("click", clearGrid);
-function clearGrid(){
-  const clearing = document.querySelectorAll("div");
-  clearing.forEach((div) => {
-    div.classList.remove("colored");
+
+colorPicker.addEventListener("input", () => {
+  const color = colorPicker.value;
+  event.target.style.backgroundColor = color;
+});
+
+// Functions for clearing the grid and coloring cells
+function clearGrid() {
+  const coloredCells = grid.querySelectorAll(".colored");
+  coloredCells.forEach((cell) => {
+    cell.classList.remove("colored");
+    cell.style.backgroundColor = "";
   });
 }
 
-const colorPicker = document.querySelector('#color-picker');
-
-colorPicker.addEventListener('input', function() {
-  const color = this.value;
-  const element = event.target;
-  element.style.backgroundColor = color;
-});
-
 function colorChange(event) {
-  event.target.classList.add("colored");
   const color = colorPicker.value;
+  event.target.classList.add("colored");
   event.target.style.backgroundColor = color;
 }
